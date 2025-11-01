ef50e542523eb810a0f4419df786886225c0ffc9
maria-schulze <164437411+maria-schulze@users.noreply.github.com>
2025-11-01 00:41:31 +0000

    seccond commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,40 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+
 
 export function Login() {
     return (
         <>
             <Container>
-                <Navbar expand="lg" className="bg-body-tertiary">
+                <Navbar className="mb-3"bg="dark" data-bs-theme="dark">
                     <Container>
-                        <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                        <Navbar.Brand href="#">Minha App</Navbar.Brand>
                     </Container>
                 </Navbar>
+            </Container>
+
+            <Container>
+                <Card>
+                    <Card.Body>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Senha</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Entrar
+                            </Button>
+                        </Form>
+                    </Card.Body>
+                </Card>
             </Container>
         </>
     );
